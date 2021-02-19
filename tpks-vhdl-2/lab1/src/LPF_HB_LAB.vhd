67d7e1c3f11b02bbec55e6a7e233fcac2236ee26
smithros <kovalr2000@gmail.com>
2021-02-19 20:04:57 +0200

    tpks-2 lab1 fix

--- tpks-vhdl-2/lab1/src/LPF_HB_LAB.vhd
+++ tpks-vhdl-2/lab1/src/LPF_HB_LAB.vhd
@@ -15,17 +15,17 @@ architecture synt of LPF_HB_LAB is
     constant a : signed(11 downto 0) := to_signed(integer(-0.156*2.0**11),12);  
     constant b : signed(11 downto 0) := to_signed(integer(0.727*2.0**11),12);
     constant c : signed(11 downto 0) := to_signed(integer(-0.269*2.0**11),12);     
-    signal qi, xi, xi_1, xi_2 : signed(19 downto 0);
+    signal yi  : signed(13 downto 0); 
+	signal xi, xi_1, xi_2       : signed(19 downto 0);
 	signal qi_1, qi_2, pi, pi_1 : signed(31 downto 0);
-	signal sm, sm_2 : signed(19 downto 0);
-    signal yi : signed(13 downto 0);    
 begin
     LPF: process(CLK,RST)
-        variable yt : signed(19 downto 0);
+	variable yt   : signed(19 downto 0); 
+	variable sm_2 : signed(19 downto 0);
+	variable qi   : signed(19 downto 0);
     begin
         if RST = '1' then
             xi   <= (others => '0');
-            qi   <= (others => '0');
             pi   <= (others => '0');
 			xi_1 <= (others => '0');
             xi_2 <= (others => '0');
@@ -33,22 +33,18 @@ begin
             qi_2 <= (others => '0');
 			pi_1 <= (others => '0');
 			yi   <= (others => '0');
-			sm   <= (others => '0');
-        elsif CLK='1' and CLK'event then    
-            xi   <= RESIZE(signed(X&"0000") , 20);
+        elsif CLK = '1' and CLK'event then    
+            xi   <= RESIZE(signed(X&"0000"), 20);
 			xi_1 <= xi;
             xi_2 <= xi_1;
-            qi   <= xi - sm;
+            qi   := xi- qi_2(30 downto 11) - pi_1(30 downto 11);
             qi_1 <= qi * b;
             qi_2 <= qi_1;
-			pi   <= qi * c;
-			pi_1 <= pi;
-			sm   <= qi_2(30 downto 11) + pi_1(30 downto 11);
-			sm_2 <= xi_2 + qi_1(30 downto 11) + pi_1(30 downto 11);
+			pi_1 <= qi * c;
+			sm_2 := xi_2 + qi_1(30 downto 11) + pi_1(30 downto 11);
             yt   := sm_2 + xi_1;
 			yi   <= yt(16 downto 3);
         end if;
     end process; 
     Y <= std_logic_vector(yi);
 end synt;
-
--- tpks-vhdl-2/lab1/src/lpf_hb_lab_TB.vhd
+++ tpks-vhdl-2/lab1/src/lpf_hb_lab_TB.vhd
@@ -3,13 +3,13 @@ use IEEE.STD_LOGIC_1164.all;
 
 entity lpf_hb_lab_TB is   
     generic(nb : natural := 14);
-    port(
-        MAGN    : out real;
-        LOGMAGN : out real;
-        PHASE   : out real;
-        ENA     : inout STD_LOGIC;
-        FREQ    : out integer
-        );
+    
+	port(MAGN    : out real;
+         LOGMAGN : out real;
+         PHASE   : out real;
+         ENA     : inout STD_LOGIC;
+         FREQ    : out integer
+    );
 end lpf_hb_lab_TB;     
 
 architecture arch of lpf_hb_lab_TB is
@@ -37,16 +37,35 @@ architecture arch of lpf_hb_lab_TB is
             );
     end component;
     
-    signal CLK, RST : std_logic := '0'; 
+    signal CLK, RST       : std_logic := '0'; 
     signal x1, x2, y1, y2 : STD_LOGIC_VECTOR(nb-1 downto 0);
 begin 
     CLOCK:     
-    RST <= '1' after 0 ns,'0' after 25 ns;
-    CLK <=  '1' after 50 ns when CLK = '0' else
-    '0' after 50 ns when CLK = '1';
-    Test1 : LPF_HB_LAB port map (CLK, RST, X => x1,Y => y1);      
-    Test2 : LPF_HB_LAB port map (CLK, RST,X => x2, Y => y2);    
-    Test3 : FilterTB port map (CLK, RST,IMO => x2, REO => x1,RERSP => y1, IMRSP => y2, MAGN => MAGN, LOGMAGN => LOGMAGN, PHASE => PHASE, ENA => ENA, FREQ => FREQ);
+    	RST <= '1' after 0 ns,'0' after 25 ns;
+    	CLK <=  '1' after 50 ns when CLK = '0' else '0' after 50 ns when CLK = '1';
+    T1 : LPF_HB_LAB port map (
+		CLK, 
+		RST, 
+		X => x1,
+		Y => y1
+	);      
+    T2 : LPF_HB_LAB port map (
+		CLK, 
+		RST,
+		X => x2, 
+		Y => y2
+	);    
+    T3 : FilterTB port map (
+		CLK, 
+		RST,
+		IMO => x2, 
+		REO => x1, 
+		RERSP => y1, 
+		IMRSP => y2, 
+		MAGN => MAGN, 
+		LOGMAGN => LOGMAGN,
+		PHASE => PHASE, 
+		ENA => ENA, 
+		FREQ => FREQ
+	);
 end arch;
-
-
